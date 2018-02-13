5c539579fb42609a4b3b20582798615c4b8e7206
SturgisRaphael <raphael.sturgis@etu.univ-amu.fr>
2018-02-13 21:48:52 +0100

    Fini

--- TP3/orderSolver.c
+++ TP3/orderSolver.c
@@ -2,6 +2,7 @@
 // Created by raphael on 2/2/18.
 //
 
+#include <zconf.h>
 #include "orderSolver.h"
 #include "order.h"
 
@@ -14,16 +15,11 @@
  * return: array[n][m]
  */
 
+#define TAUX_ERREUR 0.00001
+
 bool add_new_pattern(double *dualSolution, order o, int **patterns, int sizeOfPattern);
 
 void printIntSolution(glp_prob *p, int nbColumns) ;
-
-/*
- * sebastien.ratel@lis-lab.fr
- * objet: [M1ARO] NOM1-NOM2
- * rendu archive compressée nommée NOM1-NOM2 avec README et makefile
- * Avant le 14/02
- */
 
 void printTab(int **patterns, int p, int q){
     for(int i = 0; i < q; i++)
@@ -94,7 +90,7 @@ int **solveOrder(order o) {
     bool flag;
 
     double* dualSolution;
-    int limit = 2;
+    int limit = 10000;
 
     do {
         index = 0;
@@ -115,6 +111,7 @@ int **solveOrder(order o) {
 
         //Creating problem
         lp = glp_create_prob();
+        glp_term_out(GLP_OFF);
         glp_set_prob_name(lp, "order");
         glp_set_obj_dir(lp, GLP_MIN);
 
@@ -141,23 +138,10 @@ int **solveOrder(order o) {
 
         double* matrix = patternsConvertor(patterns, sizeOfPatterns, o.numberSizes);
 
-//        printf("sizeOfPatterns = %d\n", sizeOfPatterns);
-//
-//        printf("row_coef = \n");
-//        printList(row_coef, (sizeOfPatterns * o.numberSizes) + 1);
-//
-//        printf("col_coef = \n");
-//        printList(col_coef, (sizeOfPatterns * o.numberSizes) + 1);
-//
-//        printf("Matrix = \n");
-//        printDoubleTab(matrix, (sizeOfPatterns * o.numberSizes));
-
         //loading matrix
         glp_load_matrix(lp, (o.numberSizes * sizeOfPatterns), row_coef, col_coef, matrix);
 
         glp_simplex(lp, NULL);
-
-        glp_print_sol(lp, "sol.txt");
 
         free(matrix);
         free(row_coef);
@@ -179,9 +163,14 @@ int **solveOrder(order o) {
         limit--;
     } while (flag && limit > 0);//add_new_pattern(lp, patterns, o, sizeOfPatterns)
 
+    if(limit == 0)
+        printf("ERROR: limit atteinte\n");
+
+    printf("z = %lf\n", glp_get_obj_val(lp));
+
     printTab(patterns, sizeOfPatterns, o.numberSizes);
 
-    printSolution(lp, sizeOfPatterns + 1);
+    printf("fait en %d itaration\n", 10000 - limit);
 
     for (int i = 0; i < sizeOfPatterns; i++)
         free(patterns[i]);
@@ -203,8 +192,11 @@ bool add_new_pattern(double *dualSolution, order o, int **patterns, int sizeOfPa
 
     //Creating problem
     s = glp_create_prob();
+    glp_term_out(GLP_OFF);
     glp_set_prob_name(s, "Sac a dos");
     glp_set_obj_dir(s, GLP_MAX);
+
+
 
     //Creating Constraints
     glp_add_rows(s, 1);
@@ -227,17 +219,17 @@ bool add_new_pattern(double *dualSolution, order o, int **patterns, int sizeOfPa
 
     int *row_coef = malloc((o.numberSizes + 1) * sizeof(int));
     row_coef[0] = 0;
-    for(int j = 1; j <= o.numberSizes; j++)
+    for (int j = 1; j <= o.numberSizes; j++)
         row_coef[j] = 1;
 
     int *col_coef = malloc((o.numberSizes + 1) * sizeof(int));
-    for(int j = 1; j <= o.numberSizes; j++)
+    for (int j = 1; j <= o.numberSizes; j++)
         col_coef[j] = j;
     col_coef[0] = 0;
 
     double *matrix = malloc((o.numberSizes + 1) * sizeof(double));
     matrix[0] = 0.0;
-    for(int i = 1; i < o.numberSizes + 1; i++)
+    for (int i = 1; i < o.numberSizes + 1; i++)
         matrix[i] = o.array[i - 1][1];
 
     glp_load_matrix(s, o.numberSizes, row_coef, col_coef, matrix);
@@ -249,13 +241,11 @@ bool add_new_pattern(double *dualSolution, order o, int **patterns, int sizeOfPa
     free(row_coef);
     free(matrix);
 
-    printf("z = %lf\n", glp_mip_obj_val(s) );
-    if(glp_mip_obj_val(s) <= 1)
+    if (glp_mip_obj_val(s) <= 1.0 + TAUX_ERREUR) //Neccessaire car erreur d'arrondi
         return false;
 
     patterns[sizeOfPattern] = malloc(o.numberSizes * sizeof(int));
-    for(int i = 0; i < o.numberSizes; i++)
-    {
+    for (int i = 0; i < o.numberSizes; i++) {
         patterns[sizeOfPattern][i] = (int) glp_mip_col_val(s, i + 1);
     }
 
@@ -263,67 +253,3 @@ bool add_new_pattern(double *dualSolution, order o, int **patterns, int sizeOfPa
 
     return true;
 }
-
-/**
-bool add_new_pattern(glp_prob *pProb, int **pDouble, order o, int sizeOfPattern) {
-    glp_prob *s;
-
-    //Creating problem
-    s = glp_create_prob();
-    glp_set_prob_name(s, "Sac a dos");
-    glp_set_obj_dir(s, GLP_MAX);
-
-    //Creating Constraints
-    glp_add_rows(s, 1);
-
-    glp_set_row_name(s, 1, "constraint"); //naming constraints
-    printf("constraint\n");
-    glp_set_row_bnds(s, 1, GLP_UP, 0.0, o.initWidth);
-
-    //Creating Variables
-    glp_add_cols(s, o.numberSizes);
-    for (int i = 1; i <= o.numberSizes; i++) {
-        char variables_names[STRINGLENGHT];
-        sprintf(variables_names, "a_%d", i);
-        printf(variables_names);
-        printf("\n");
-        glp_set_col_bnds(s, i, GLP_LO, 0.0, 0.0);
-
-        glp_set_obj_coef(s, i, glp_get_row_dual(pProb, i));
-    }
-
-    int *row_coef = malloc((o.numberSizes + 1) * sizeof(int));
-    row_coef[0] = 0;
-    for(int j = 1; j <= o.numberSizes; j++)
-        row_coef[j] = 1;
-
-    int *col_coef = malloc((o.numberSizes + 1) * sizeof(int));
-    for(int j = 0; j <= o.numberSizes; j++)
-        col_coef[j] = j;
-
-    double *matrix = malloc((o.numberSizes + 1) * sizeof(double));
-    matrix[0] = 0.0;
-    for(int i = 1; i < o.numberSizes + 1; i++)
-        matrix[i] = o.array[i - 1][1];
-
-    glp_load_matrix(s, o.numberSizes, row_coef, col_coef, matrix);
-
-    glp_simplex(s, NULL);
-
-    printSolution(s, o.numberSizes);
-
-    if(glp_get_obj_val(s) <= 1)
-        return false;
-
-    pDouble = realloc(pDouble, (sizeOfPattern + 1) * sizeof(double*));
-    pDouble[sizeOfPattern + 1] = malloc(o.numberSizes * sizeof(double));
-    for(int i = 0; i < o.numberSizes; i++)
-    {
-        pDouble[sizeOfPattern + 1][i] = (int) glp_get_col_prim(s, i + 1);
-    }
-
-    glp_delete_prob(s);
-
-    return true;
-}
-*/
